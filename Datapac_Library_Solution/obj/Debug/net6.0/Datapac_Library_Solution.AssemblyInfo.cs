cf92e95117bb9e14c159526eca1cd57d1d38a373
monikavlckova <36670626+monikavlckova@users.noreply.github.com>
2024-07-14 22:36:30 +0200

    ciarka v appsettings

--- Datapac_Library_Solution/obj/Debug/net6.0/Datapac_Library_Solution.AssemblyInfo.cs
+++ Datapac_Library_Solution/obj/Debug/net6.0/Datapac_Library_Solution.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Datapac_Library_Solution")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b79b9fc09de2c4f03a026fb224de6b6d66409c86")]
 [assembly: System.Reflection.AssemblyProductAttribute("Datapac_Library_Solution")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Datapac_Library_Solution")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
